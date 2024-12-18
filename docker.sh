521f5cb3fe4d9a779e7ddcef1f67d16ac8e65720
Adrià Muro <itsblazkn@gmail.com>
2024-12-18 03:03:37 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Actualitza els paquets del sistema
+# Actualitza els repositoris del sistema
 sudo apt-get update
 
 # Instal·la les dependències necessàries
@@ -9,14 +9,17 @@ sudo apt-get install -y ca-certificates curl
 # Crea el directori per a les claus GPG
 sudo install -m 0755 -d /etc/apt/keyrings
 
-# Afegeix la clau GPG oficial de Docker
+# Descarrega i afegeix la clau GPG oficial de Docker
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Afegeix el repositori de Docker a les fonts d'Apt
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Actualitza la llista de repositoris
+# Torna a actualitzar la llista de repositoris
 sudo apt-get update
 
 # Instal·la els paquets de Docker
